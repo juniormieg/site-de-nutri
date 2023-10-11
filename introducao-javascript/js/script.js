6b08be3c077aa893cc1657fb517ec60318dc0510
junior <miegoverdadeiro@gmail.com>
2023-10-11 08:54:40 -0300

    add to list

--- introducao-javascript/js/script.js
+++ introducao-javascript/js/script.js
@@ -33,6 +33,35 @@ for (var i = 0; i < pacientes.length; i++) {
 }
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function () {
-  console.log("cliqueiiii");
+botaoAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  var form = document.querySelector("#form-adiciona");
+
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var gordura = form.gordura.value;
+  var altura = form.altura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  gorduraTd.textContent = gordura;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(alturaTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
 });
